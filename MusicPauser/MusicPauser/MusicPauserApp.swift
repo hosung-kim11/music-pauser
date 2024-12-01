ef49fd7cf4f20042d72d9fc4d627982651041e3a
Hosung Kim <hk63@rice.edu>
2024-12-01 15:36:01 -0600

    initial commit

---
+++ MusicPauser/MusicPauser/MusicPauserApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPauserApp.swift
+//  MusicPauser
+//
+//  Created by Hosung Kim on 12/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MusicPauserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
